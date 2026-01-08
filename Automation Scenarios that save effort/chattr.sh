c0a87c57634cc17702e1078abd285e5e9efcc06c
icey <1944659866@qq.com>
2026-01-08 19:19:49 +0800

    Delete Automation Scenarios that save effort directory

--- Automation Scenarios that save effort/chattr.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo "List of folders available to change attribute for immuatability!"
-ls -ltu /home/ubuntu/Desktop/
-read -p "Enter folder to make immuatable: " f
-sudo chattr +i $f
-echo "Folder made immutable" 
-
--- Automation Scenarios that save effort/useradd.sh
+++
@@ -1,4 +0,0 @@
-read -p "Enter the name of the user you want to create: " name
-adduser $name
-echo "User created"
- 
